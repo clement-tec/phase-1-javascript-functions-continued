d2ec95849916940597b6bd71e38bad71a698bb1c
clement senou <clementsenou74@gmail.com>
2023-01-30 22:12:17 -0600

    complet

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+function mondayWork(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+function wrapAdjective(num="*") {
+    return function(adjective="special") {
+        return `You are ${num}${adjective}${num}!`
+    }
+}
